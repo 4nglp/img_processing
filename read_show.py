cdb0d1a197964720b577dc1e704996ac441ed2c5
4nglp <4nglpw@gmail.com>
2025-11-01 17:15:21 +0100

    init, reading and showing simple images

---
+++ read_show.py
@@ -0,0 +1,14 @@
+import cv2, os
+
+# defining the path
+img_path = os.path.join('imgs', 'serious-cat.jpg')
+# then we reading 
+cat = cv2.imread(img_path)
+# doing all the prev steps in a single cmd
+dog = cv2.imread('imgs/cooked-dog.jpg')
+# after that  we showing
+cv2.imshow('Cat', cat)
+cv2.imshow('Dog', dog)
+# closing after a key is pressed
+cv2.waitKey(0)
+cv2.destroyAllWindows()
